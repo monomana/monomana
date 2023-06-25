66993b1f43f95f8315068b361e9a921c598d4f60
mjat <mjat.util@gmail.com>
2023-06-25 08:46:53 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// $("#btn").click(function() {
+// 	$('.shrinker').removeClass('timelapse');
+// 	setTimeout(function() {
+// 		$('.shrinker').addClass('timelapse');
+// 	}, 500)
+// })
+
+/* progress bar gradient */
+/* document.querySelector("#btn")
+.addEventListener("click",event => {
+    const p= document.querySelector(".shrinker")
+    p.classList.remove("timelapse");
+    setTimeout(function() {
+        p.classList.add('timelapse');
+	}, 500)
+}) */
